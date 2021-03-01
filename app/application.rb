8399bcad5130a33537539fae6389c8f157b6f996
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-01 00:14:00 -0500

    commit tree 7fe48b0189798ab56bade24ddb905a5cc2b93164

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      search_item = req.path.split("/items/").last
+      
+
+      if @@items.map {|item| item.name}.include?(search_item)
+        search_item = Item.find(search_item)
+        resp.write search_item.price
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,25 @@
 class Item
   attr_accessor :name, :price
 
+  @@items = []
+
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
+  end
+
+  def self.all
+    @@items
+  end
+
+  def self.all_names
+    self.all.map {|item| item.name}
+  end
+
+  def self.find(search_name)
+    found_item = nil
+    self.all.each {|item| found_item = item if item.name == search_name}
+    found_item
   end
 end
